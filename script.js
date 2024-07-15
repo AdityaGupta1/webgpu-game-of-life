d04c64ce91552cb4e8d9803f0acc73d8d8a71b82
AdityaGupta1 <adityag@gmail.com>
2024-07-14 22:04:02 -0700

    wacky colorful grid

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const GRID_SIZE_X = 64;
+const GRID_SIZE_Y = 64;
+const CELL_PADDING = 0.1;
+
 const canvas = document.querySelector("canvas");
 
 if (!navigator.gpu) {
@@ -19,21 +23,19 @@ context.configure({
 });
 
 const vertices = new Float32Array([
-    -0.8, -0.8, // Triangle 1 (Blue)
-    0.8, -0.8,
-    0.8,  0.8,
+    CELL_PADDING, CELL_PADDING,
+    1 - CELL_PADDING, CELL_PADDING,
+    1 - CELL_PADDING, 1 - CELL_PADDING,
 
-    -0.8, -0.8, // Triangle 2 (Red)
-    0.8,  0.8,
-    -0.8,  0.8,
+    CELL_PADDING, CELL_PADDING,
+    1 - CELL_PADDING, 1 - CELL_PADDING,
+    CELL_PADDING, 1 - CELL_PADDING,
 ]);
-
 const vertexBuffer = device.createBuffer({
     label: "cell vertices",
     size: vertices.byteLength,
     usage: GPUBufferUsage.VERTEX | GPUBufferUsage.COPY_DST,
 });
-
 device.queue.writeBuffer(vertexBuffer, /*bufferOffset=*/0, vertices);
 
 const vertexBufferLayout = {
@@ -45,22 +47,68 @@ const vertexBufferLayout = {
     }],
 };
 
-const cellVertShader = device.createShaderModule({
-    label: "cell vert shader",
-    code: `
-@vertex
-fn vertMain(@location(0) pos: vec2f) -> @builtin(position) vec4f {
-    return vec4f(pos, 0, 1);
-}
-    `
+const gridSizeUniformArray = new Uint32Array([GRID_SIZE_X, GRID_SIZE_Y]);
+const gridSizeUniformBuffer = device.createBuffer({
+  label: "grid size uniform",
+  size: gridSizeUniformArray.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
 });
+device.queue.writeBuffer(gridSizeUniformBuffer, 0, gridSizeUniformArray);
 
-const cellFragShader = device.createShaderModule({
-    label: "cell frag shader",
+const cellPaddingUniformArray = new Float32Array([CELL_PADDING]);
+const cellPaddingUniformBuffer = device.createBuffer({
+  label: "cell padding uniform",
+  size: cellPaddingUniformArray.byteLength,
+  usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+device.queue.writeBuffer(cellPaddingUniformBuffer, 0, cellPaddingUniformArray);
+
+const cellShader = device.createShaderModule({
+    label: "cell shader",
     code: `
+@group(0) @binding(0) var<uniform> gridSize: vec2u;
+@group(0) @binding(1) var<uniform> cellPadding: f32;
+
+struct VertexInput {
+    @location(0) pos: vec2f,
+    @builtin(instance_index) instanceIdx: u32,
+};
+
+struct VertexOutput {
+    @builtin(position) pos: vec4f,
+    @location(0) @interpolate(flat) cellGridPos: vec2u,
+};
+
+@vertex
+fn vertMain(in: VertexInput) -> VertexOutput {
+    let cellGridPos = vec2u(in.instanceIdx % gridSize.x, in.instanceIdx / gridSize.x);
+    let inverseGridSize = 1 / vec2f(gridSize);
+
+    var vertPos = ((vec2f(cellGridPos) + in.pos) * inverseGridSize) * 2 - 1;
+    vertPos *= 1 - 2 * (cellPadding * inverseGridSize);
+
+    var out: VertexOutput;
+    out.pos = vec4(vertPos, 0, 1);
+    out.cellGridPos = cellGridPos;
+    return out;
+}
+
+struct FragInput {
+    @location(0) @interpolate(flat) cellGridPos: vec2u,
+};
+
+struct FragOutput {
+    @location(0) color: vec4f,
+}
+
 @fragment
-fn fragMain() -> @location(0) vec4f {
-    return vec4(1, 0, 1, 1);
+fn fragMain(in: FragInput) -> FragOutput {
+    let normalizedCellPos = vec2f(in.cellGridPos) / vec2f(gridSize);
+    var color = vec4f(normalizedCellPos, 1 - normalizedCellPos.x, 1);
+
+    var out: FragOutput;
+    out.color = color;
+    return out;
 }
     `
 });
@@ -69,17 +117,32 @@ const cellPipeline = device.createRenderPipeline({
     label: "cell pipeline",
     layout: "auto",
     vertex: {
-        module: cellVertShader,
+        module: cellShader,
         entryPoint: "vertMain",
         buffers: [vertexBufferLayout]
     },
     fragment: {
-        module: cellFragShader,
+        module: cellShader,
         entryPoint: "fragMain",
         targets: [{
             format: canvasFormat
         }]
     }
+});
+
+const cellBindGroup = device.createBindGroup({
+    label: "cell renderer bind group",
+    layout: cellPipeline.getBindGroupLayout(0),
+    entries: [
+        {
+            binding: 0,
+            resource: { buffer: gridSizeUniformBuffer }
+        },
+        {
+            binding: 1,
+            resource: { buffer: cellPaddingUniformBuffer }
+        }
+    ],
 });
 
 const encoder = device.createCommandEncoder();
@@ -95,7 +158,10 @@ const pass = encoder.beginRenderPass({
 
 pass.setPipeline(cellPipeline);
 pass.setVertexBuffer(0, vertexBuffer);
-pass.draw(vertices.length / 2);
+
+pass.setBindGroup(0, cellBindGroup);
+
+pass.draw(vertices.length / 2, GRID_SIZE_X * GRID_SIZE_Y);
 
 pass.end();
 
